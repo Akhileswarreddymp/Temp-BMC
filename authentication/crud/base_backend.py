9b55fc6e6cb7ad10d6872d01aad008629099b4b6
Akhileswarreddymp <akhileswarreddymp@gmail.com>
2025-03-10 11:55:40 +0530

    base backend

--- authentication/crud/base_backend.py
+++ authentication/crud/base_backend.py
@@ -1,10 +1,8 @@
-import pydantic
+from pydantic import BaseModel
 from sqlmodel import Session
 
 
 class BaseBackend:
-    def __init__(
-        self, session: Session, concrete_type: type[pydantic.BaseModel]
-    ) -> None:
+    def __init__(self, session: Session, concrete_type: type[BaseModel]) -> None:
         self.session = session
         self.concrete_type = concrete_type
